3fea4be9ebfd8bf3eff267f3798c8ac0a81d243b
maxzog <maxzog@umich.edu>
2024-10-25 20:52:16 -0400

    Particle diameter groups added

--- examples/dns_channel/src/simulation.f90
+++ examples/dns_channel/src/simulation.f90
@@ -61,6 +61,11 @@ module simulation
    logical :: restarted
    type(event) :: save_evt
 
+   !> Particles
+   integer :: ngroups
+   integer :: np_per_group,np
+   real(WP), dimension(:), allocatable :: group_diameters
+
 contains
 
    !> Specialized subroutine that outputs particle concentration statistics
@@ -227,7 +232,7 @@ contains
 
    !> Initialization of problem solver
    subroutine simulation_init
-      use param, only: param_read
+      use param, only: param_read,param_getsize
       implicit none
 
 
@@ -331,9 +336,6 @@ contains
          type(bcond), pointer :: mybc
          ! Create flow solver
          fs=incomp(cfg=cfg,name='NS solver')
-         ! Define boundary conditions
-         call fs%add_bcond(name='bottom',type=dirichlet,locator=bottom_of_domain,face='y',dir=-1,canCorrect=.false.)
-         call fs%add_bcond(name='top',type=dirichlet,locator=top_of_domain,face='y',dir=+1,canCorrect=.false. )
          ! Assign constant viscosity
          call param_read('Dynamic viscosity',visc); fs%visc=visc
          ! Assign constant density
@@ -363,25 +365,14 @@ contains
             do k=fs%cfg%kmino_,fs%cfg%kmaxo_
                do j=fs%cfg%jmino_,fs%cfg%jmaxo_
                   do i=fs%cfg%imino_,fs%cfg%imaxo_
-                     !if (fs%umask(i,j,k).eq.0) fs%U(i,j,k)=fs%U(i,j,k)+amp*vel*cos(16.0_WP*twoPi*fs%cfg%zm(k)/fs%cfg%zL)!*random_normal(m=0.0_WP,sd=0.5_WP)
-                     !if (fs%wmask(i,j,k).eq.0) fs%W(i,j,k)=fs%W(i,j,k)+amp*vel*cos(16.0_WP*twoPi*fs%cfg%xm(i)/fs%cfg%xL)!*random_normal(m=0.0_WP,sd=0.5_WP)
+                    ! if (fs%umask(i,j,k).eq.0) fs%U(i,j,k)=fs%U(i,j,k)+amp*vel*cos(16.0_WP*twoPi*fs%cfg%zm(k)/fs%cfg%zL)
+                    ! if (fs%wmask(i,j,k).eq.0) fs%W(i,j,k)=fs%W(i,j,k)+amp*vel*cos(16.0_WP*twoPi*fs%cfg%xm(i)/fs%cfg%xL)
                      if (fs%umask(i,j,k).eq.0)fs%U(i,j,k)=fs%U(i,j,k)+Ubulk*random_uniform(lo=-0.5_WP*amp,hi=0.5_WP*amp)+amp*Ubulk*cos(8.0_WP*twoPi*fs%cfg%zm(k)/fs%cfg%zL)*cos(8.0_WP*twoPi*fs%cfg%ym(j)/fs%cfg%yL)
                      if (fs%wmask(i,j,k).eq.0)fs%W(i,j,k)=fs%W(i,j,k)+Ubulk*random_uniform(lo=-0.5_WP*amp,hi=0.5_WP*amp)+amp*Ubulk*cos(8.0_WP*twoPi*fs%cfg%xm(i)/fs%cfg%xL)
                   end do
                end do
             end do
          end if
-         ! Set no-slip walls
-         call fs%get_bcond('bottom',mybc)
-         do n=1,mybc%itr%no_
-            i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
-            fs%V(i,j,k)=0.0_WP
-         end do
-         call fs%get_bcond('top',mybc)
-         do n=1,mybc%itr%no_
-            i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
-            fs%V(i,j,k)=0.0_WP
-         end do
          ! Calculate cell-centered velocities and divergence
          call fs%interp_vel(Ui,Vi,Wi)
          call fs%get_div()
@@ -393,24 +384,34 @@ contains
          use string, only: str_medium
          use random, only: random_uniform, random_normal
          real(WP) :: dp
-         integer :: i,np
+         integer :: i,group
          character(len=str_medium) :: timestamp
          ! Create solver
          lp=lpt(cfg=cfg,name='LPT')
-         ! Get drag model from the inpit
-         ! call param_read('Drag model',lp%drag_model,default='Schiller-Naumann')
          ! Get particle density from the input
          call param_read('Particle density',lp%rho)
-         ! Get particle diameter
-         call param_read('Particle diameter',dp)
-         ! Get number of particles
-         call param_read('Number of particles',np)
+         ! Get number of particle groups
+         ngroups=param_getsize('Particle diameters')
+         ! Allocate diameter array
+         allocate(group_diameters(1:ngroups))
+         ! Get particle diameters
+         call param_read('Particle diameters',group_diameters)
+         ! Get number of particles per group
+         call param_read('Particles per group',np_per_group)
+         ! Compute total number of particles
+         np=ngroups*np_per_group
+         ! Initialize group index
+         group=0
          ! Randomly distribute particles w/o checking for overlap
          if (lp%cfg%amRoot) then
             call lp%resize(np)
             do i=1,np
                ! Give id
                lp%p(i)%id=int(i,8)
+               ! Increment group counter
+               if (mod(i,np_per_group).eq.1) group=group+1
+               ! Get diameter
+               dp=group_diameters(group)
                ! Set the diameter
                lp%p(i)%d=dp
                ! Assign random position in the domain
@@ -433,8 +434,7 @@ contains
       
       ! Create partmesh object for Lagrangian particle output
       create_pmesh: block
-         integer :: np,i
-         call param_read('Number of particles',np)
+         integer :: i
          pmesh=partmesh(nvar=1,nvec=3,name='lpt')
          pmesh%varname(1)="id"
          pmesh%vecname(1)="vel"
@@ -609,23 +609,6 @@ contains
 
             ! Apply other boundary conditions on the resulting fields
             call fs%apply_bcond(time%t,time%dt)
-
-            ! Reset Dirichlet BCs
-            dirichlet_velocity: block
-              use incomp_class, only: bcond
-              type(bcond), pointer :: mybc
-              integer :: n,i,j,k
-              call fs%get_bcond('bottom',mybc)
-              do n=1,mybc%itr%no_
-                 i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
-                 fs%V(i,j,k)=0.0_WP
-              end do
-              call fs%get_bcond('top',mybc)
-              do n=1,mybc%itr%no_
-                 i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
-                 fs%V(i,j,k)=0.0_WP
-              end do
-            end block dirichlet_velocity
 
             ! Solve Poisson equation
             call fs%correct_mfr()
